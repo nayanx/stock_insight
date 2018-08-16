bdbaa48c1e764a01b2dbd29b6cb8980ac0fe125d
PARI <nayanx@gmail.com>
2018-08-17 01:08:22 +0530

    initial

---
+++ testGithub.py
@@ -0,0 +1,54 @@
+# lessons till strings
+# a = [100,200,300] #lists
+# b = (100,200,30) #tuples
+# c = {'a':11,'b':22,'c':65} #dictionary
+# print(a)
+# a.append(400)
+# print(a)
+# print(c['b'])
+#
+# if 100 > 200 :
+#     print('No')
+# else: print("yes")
+# i=0
+# while i < 7:
+#     print(i)
+#     i=i+1
+# # i=i+4
+#     i=i+5 # indentation is important
+# str=('abcdefgh')
+# print(str[0:5])
+# print(str[0:5:3]) #third param is step
+# print(str.find('c'))
+# print(str.count('c'))
+# print(str.replace('c','f'))
+
+#list methods. Vector functionsself.
+# from datetime import datetime
+# v1 = ['1','2','3','8']
+# v2 = ['4','5','6','7']
+# print (v1+v2)
+# str1=('   a11,b,c,d,e,f   ')
+# str2=('    a    ')
+# v4=str1.split(',')
+# print (v4)
+#
+# if 'd' in v4:
+#     print('yes')
+# else:
+#     print('no')
+#
+# # s=input('--')
+# # print (len(s))
+# tm=datetime.now()
+# print (list(zip(v1,v2)))
+# print (len(str2.rstrip()))
+# print (tm.strftime("%Y"))
+#
+def printmenu():
+    print("this is a menu")
+
+printmenu()
+
+# parte tres
+# pypi.org
